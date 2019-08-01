08064bfa03862d04db9d6a174ca5fc6f538eb4e5
RZFalcore <box.rz.box@gmail.com>
2019-08-01 17:25:26 +0300

    Updates

---
+++ precache-manifest.6b4570ac329f6d7a1ad848565f72e91f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60a13e6541cace3b4db6a2889548d368",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "330b9aa8b2cf82c1ae62",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.79577e37.chunk.js"
+  },
+  {
+    "revision": "052338c15f0cfa810fb3",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime~main.a33a0e9d.js"
+  }
+]);
